062b76d32e342ec42d50d953ba246b1c3e7b8686
Zach <zcervi@qw-corp.com>
2020-12-01 19:10:34 -0700

    added text to home screen

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, StatusBar, Image, Dimensions } from 'react-native';
-
+import { View, StyleSheet, StatusBar, Image, Dimensions, Text } from 'react-native';
+import { format } from 'date-fns';
 import colors from '../constants/colors';
 import { ConversionInput } from '../components/ConversionInput';
 const screen = Dimensions.get('window');
@@ -23,10 +23,26 @@ const styles = StyleSheet.create({
         position: 'absolute',
         width: screen.width * 0.25,
         height: screen.width * 0.25
+    },
+    textHeader: {
+        color: colors.white,
+        fontWeight: 'bold',
+        fontSize: 30,
+        marginVertical: 20,
+        textAlign: 'center'
+    },
+    text: {
+        color: colors.white,
+        fontSize: 13,
+        textAlign: 'center'
     }
 
 });
 export default () => {
+    const baseCurrency = 'USD';
+    const quoteCurrency = 'GBP';
+    const conversionRate = 0.89824;
+    const date = new Date();
     return (
         <View style={styles.container}>
             <StatusBar barStyle='light-content' backgroundColor={colors.blue}></StatusBar>
@@ -34,20 +50,23 @@ export default () => {
                 <Image source={require('../assets/images/background.png')} style={styles.logoBackground} resizeMode="contain"></Image>
                 <Image source={require('../assets/images/logo.png')} style={styles.logo} resizeMode="contain"></Image>
             </View>
-
+            <Text style={styles.textHeader}>Currency Converter</Text>
             <ConversionInput
-                text="USD"
+                text={baseCurrency}
                 value="123"
                 onButtonPress={() => alert('todo!')}
-                onChangeText = {text => console.log('text', text)}
+                onChangeText={text => console.log('text', text)}
                 keyboardType='numeric' />
 
             <ConversionInput
-                text="GBP"
+                text={quoteCurrency}
                 value="123"
                 onButtonPress={() => alert('todo!')}
                 editable={false}
-                />
+            />
+            <Text style={styles.text}>
+                {`1 ${baseCurrency} = ${conversionRate} ${quoteCurrency} as of ${format(date, 'MMMM do, yyyy')}`}
+            </Text>
         </View>
     );
 };
